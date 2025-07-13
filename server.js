04f3159a4cc802901d781d38bdedaa074a9d6f2a
Davi Snaider <davisnaider06@gmail.com>
2025-07-12 22:48:06 -0300

    Implementando Rate Limiting

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const pool = require('./src/config/db');
 require('dotenv').config();
-const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes')
 const taskRoutes = require('./src/routes/taskRoutes')
 const { errorHandler } = require('./src/middlewares/errorMiddleware');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(cors());
+
 app.use(express.json());
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15 min
+    max : 100, //limite de 100 req  por ip
+    message: 'Muitas requisições deste IP, por favor, tente novamente após 15 minutos'
+});
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: 'Muitas tentativas de autenticação neste IP, por favor, tente novamente daqui 15 minutos' // limite de 5 requisiçoes de login
+})
+
+app.use(apiLimiter);
 
 app.get('/', (req, res) => {
     res.send('API de Gerenciamento de Tarefas está online!');
@@ -37,7 +53,7 @@ app.use(cors());
 
 //Usar as rotas de autenticação
 // Todas as rotas definidas em authRoutes.js vao ser prefixadas com /api/auth
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authLimiter, authRoutes);
 
 //Usar as rotas de tarefas
 //Todas as rotas definidas em taskRoutes.js serão prefixadas com /api/tasks
